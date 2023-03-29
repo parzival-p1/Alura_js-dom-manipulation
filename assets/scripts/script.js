e1180021086092915bf402eb1fa9576eb0fc1ffe
Paco Hdez <francj.hdez@gmail.com>
2023-03-28 22:12:15 -0600

    clase03 completada

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,8 +5,25 @@ console.log(btn);
 const createTask = (evento) => { //* arrow func o func anonima */
     evento.preventDefault();
     const input = document.querySelector("[data-form-input]");
-    console.log(input.value);
+    const value = input.value;
+    const list = document.querySelector("[data-list]")
+    const task = document.createElement("li"); //^crea el elm <li>
+    task.classList.add("card"); //^ adds css class
+    input.value = ""; //^ limpia el input
+    //* Bacticks for adding html content to show
+    const content = 
+    `<div>
+        <i class="far fa-check-square icon"></i>
+        <span class="task">${value}</span>
+    </div>
+    <i class="fas fa-trash-alt trashIcon icon"></i>`;
+    task.innerHTML = content; //^add html content
+
+    //* Add task to te li element
+    list.appendChild(task   );
+    console.log(content);
 }
+console.log(btn);
 //* Listener
 btn.addEventListener("click", createTask); //* Metódo qué genera un listener
 
